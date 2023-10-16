3a305d064ce22819763bb5ce6179f5fa163e0447
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-10-16 11:56:40 +0800

    tips card

--- lib/base_page.dart
+++ lib/base_page.dart
@@ -5,6 +5,7 @@ class BasePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+  
         backgroundColor: Colors.white,
         title: Text(
           'Dell Spark City',
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dellsparkcity/insights.dart';
+//import 'package:dellsparkcity/tips_card.dart';
 import 'package:flutter/material.dart';
-import 'base_page.dart';
+//import 'base_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,9 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BasePage(),
-      debugShowCheckedModeBanner:
-          false, // Set to false to hide the debug banner
+      home: InsightsPage(),
+      debugShowCheckedModeBanner:false, // Set to false to hide the debug banner
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           elevation: 0, // Set elevation to 0 to remove the shadow effect
