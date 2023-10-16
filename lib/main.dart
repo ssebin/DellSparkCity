efe64032c72118cb8aa3cb4b2dbc53357f84f6da
Sebin Hwang <Sebin.Hwang@dell.com>
2023-10-17 03:38:19 +0800

    removed unnecessary stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
 import 'package:dellsparkcity/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'base_page.dart';
 import 'signin.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,18 +17,4 @@ class MyApp extends StatelessWidget {
       home: const SignInScreen(), // click continue with google button
     );
   }
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: "AIzaSyAi2_MQvfgXqtBKKXFZieoHN9mDsWmSMD8",
-        authDomain: "sparkcity-e1a0a.firebaseapp.com",
-        projectId: "sparkcity-e1a0a",
-        storageBucket: "sparkcity-e1a0a.appspot.com",
-        messagingSenderId: "700048086044",
-        appId: "1:700048086044:web:2c86160609b9745afdbbd9"),
-  );
-  runApp(MyApp());
 }
