fad1d60da724d2367fa92367ceb8e9c4b17772ef
thaya1406 <66481762+thaya1406@users.noreply.github.com>
2023-10-16 13:57:00 +0800

    Stashh

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
